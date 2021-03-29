5c36a31c0903a02837b50f82ab254685770ed6fe
franklinfargoj <franklinfargoj1991@gmail.com>
2021-03-29 22:27:43 +0530

    Added category list

--- resources/views/admin/category/add.blade.php
+++ resources/views/admin/category/add.blade.php
@@ -1,3 +1,7 @@
+<div class="column" style="margin-left:450px;margin-top:12px"> 
+    <a href="{{ url('show_cat') }}"> <h5>List category</h5></a>
+</div>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -6,7 +10,7 @@
                     <form method="POST" action="{{ route('save_cat') }}">
                         @csrf
                         Add category
-
+                        <br>
                         <input type="text" name="category" id="category">
                         <button type="submit" class="btn btn-primary">
                                     Add
